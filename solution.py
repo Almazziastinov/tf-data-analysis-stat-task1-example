53817fbea2796b5363d6d308a2142e509495b687
Almazziastinov <90499168+Almazziastinov@users.noreply.github.com>
2023-04-08 00:45:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,5 @@ import numpy as np
 chat_id = 635124229 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x_39 = x - 39 
-    cur_sum = 0 
-    for i in range(len(x)):
-        cur_sum+=np.log(abs(x_39[i]))
-    mean_ = cur_sum / len(x)
-    return mean_ # Ваш ответ
+   logs = np.log(x-39)
+   return np.mean(logs) # Ваш ответ
